9ea688aaefba5eb6d4378cb1d52c24cd0c84463c
tpo-aeclarke <aeclarke@travelers.com>
2021-02-09 14:14:38 -0500

    Added meets requirements to scripts.spec

---
+++ js/scripts.spec.js
@@ -0,0 +1,25 @@
+/**
+ * MEETS EXPECTATIONS REQUIREMENTS:
+ * 12 random users are pulled from the API in a single request
+ * 
+ * New random employee information displays each time the page refreshes
+ * 
+ * The directory displays 12 users from the Random User API, and includes the following for each user:
+ *    Employee Image
+ *    First and Last Name
+ *    Email
+ *    City or location
+ * 
+ * Modal window displays at least the following details:
+ *    Employee image
+ *    Name
+ *    Email
+ *    City or location
+ *    Cell Number
+ *    Detailed Address, including street name and number, state or country, and post code.
+ *    Birthday
+ *    There is a way to close the modal window
+ * 
+ * The major elements of the directory and modal window are in place and roughly match the mockups.
+ * Note: Modal window address can have the state's full name, rather than the two letter state code
+ */
